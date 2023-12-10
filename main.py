973dff2018c7074115498dcc0ca836ad8f90b4e7
fifthace63 <rzalone@gmail.com>
2023-12-10 13:22:59 +0000

    add some data types

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
 name = input("What is your name? ")
-print("Hello, " + name)
+print("Hello, "+name+", I'm gonna teach you data types in python.")
+print("Let's start with numbers.")
+print("firs, let's talk about integers.")
+# Data Types
+# Numbers
+# Integers
+intnum = 5
+print(intnum)
+print(type(intnum))
+
+# Floats
+floatnum = 5.5
+print(floatnum)
+print(type(floatnum))
+
+# Booleans
+boolcar = True
+print(boolcar)
+print(type(boolcar))
+
+# Strings
+# Lists
+# Tuples
+# Dictionaries
+# Sets
+# Ranges
+# Functions
+# Classes
+# Modules
+# Packages
+# Exceptions
+# Regular Expressions
+# Binary Data
+# Byte Data
+# Bytes Data
